a50b8137b5cb0b4033d3dccafaf739df04a695bf
subhamoy-burman <subhamoy.2.burman@gmail.com>
2025-10-14 22:35:11 +0530

    binary tree - balance

--- src/tree/binary_tree.py
+++ src/tree/binary_tree.py
@@ -12,3 +12,19 @@ class BinaryTree:
            right_height = _height(node.right)
            return 1 + max(left_height, right_height)
        return _height(self.root)
+    
+    def is_balanced(self):
+       def check(node):
+          if node is None:
+           return 0
+          leftBalance = check(node.left)
+          if leftBalance == -1:
+            return -1
+          rightBalance = check(node.right)
+          if rightBalance == -1:
+            return -1
+          if abs(leftBalance - rightBalance) > 1:
+             return -1
+          return 1 + max(rightBalance,leftBalance)
+       return check(self.root)!= -1
+       
